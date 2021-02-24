7aa9aaa6c3fe1d94771949c101c4351daa1b3675
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2021-02-24 10:37:14 -0300

    Adiciona código que gera grafo de coautorias de partidos

---
+++ reports/grafo.R
@@ -0,0 +1,25 @@
+#' @title Cria o grafo
+#' @description Recebe dois dataframes correspondendo aos nós e arestas
+#' e gera um grafo
+#' @param nodes Nós do grafo
+#' @param edges Arestas do grafo
+#' @return Grafo com nós e arestas
+generate_graph <- function(nodes, edges) {
+  library(networkD3)
+  
+  fn <- forceNetwork(
+    Links = edges, 
+    Nodes = nodes,
+    Source = "source", 
+    Target = "target",
+    Value = "peso_total_arestas", 
+    NodeID = "partido",
+    Group ="partido", 
+    zoom = T,
+    linkColour = "#bfbdbd",
+    fontFamily = "roboto",
+    fontSize = 6,
+    opacity = 0.8)
+  
+  return(fn)
+}
