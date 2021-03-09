c0ed3d2d55cc30f71c51a9cb737c079dd89cbbf1
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2021-03-09 19:12:15 -0300

    Atualiza reports 1-1 a 1-3 para nomenclatura de convergência

--- reports/escalas.R
+++ reports/escalas.R
@@ -1,8 +1,8 @@
 escala_sinais = scale_fill_manual(
   na.value = "#dddddd",
   values = c(
-    "Positivo" = "#91cf60",
-    "Negativo" = "#fc8d59",
+    "Convergente" = "#91cf60",
+    "Divergente" = "#fc8d59",
     "Neutro" = "#ffff8a"
   ) 
 ) 
@@ -10,8 +10,8 @@ escala_sinais = scale_fill_manual(
 escala_cor_sinais = scale_color_manual(
   na.value = "#dddddd",
   values = c(
-    "Positivo" = "#91cf60",
-    "Negativo" = "#fc8d59",
+    "Convergente" = "#91cf60",
+    "Divergente" = "#fc8d59",
     "Neutro" = "#d9d975"
   ) 
 ) 
